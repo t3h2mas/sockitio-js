00e188eea823a0e2d3682b3d445a58dda33c9148
Thomas <jtruant@gmx.com>
2016-03-02 20:41:31 -0700

    hello IRC world!

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,31 @@
 var socket = io();
 $(document).ready(function() {
+
+  var nick, server, chan;
+
+  $('#userInfo').submit(function (event) {
+    event.preventDefault();
+
+    nick = $('#nick').val();
+    server = $('#server').val();
+    chan = $('#chan').val();
+
+    var resp = {nick: nick, server: server, chan: chan};
+    socket.emit('user info', resp);
+
+    $('.overlay').remove();
+    $('.overlay-message').remove();
+  })
+
   $('.f').submit(function(event) {
     event.preventDefault();
-    alert($('#m').val());
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+  });
+
+  socket.on('chat message', function(msg) {
+    // TODO: move user messages to client.
+    var $new = $('<li />').text(msg);
+    $('#messages').append($new);
   });
 });
--- server.js
+++ server.js
@@ -3,8 +3,44 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var irc = require('irc');
+
+var client; // TODO: don't use global
+
+function bindClient(c) {
+  // irc client callbacks
+
+  c.on('error', function(msg) {
+    console.log('irc.Client error: ' + msg);
+  });
+
+  c.on('message', function(from, to, message) {
+    m = from + ' => ' + to + ': ' + message;
+    console.log(m);
+    io.emit('chat message', m);
+  });
+}
+
 io.on('connection', function(socket) {
   console.log('connection established');
+
+  socket.on('user info', function(info) {
+    // this should be called first; info from modal form
+    client = new irc.Client(info.server, info.nick, { channels: [info.chan] });
+    console.log('irc client established: ');
+    bindClient(client);
+
+  })
+
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+
+    // send the msg to the client(s)
+    // TODO: consider explicit msg names
+    client.say('##socketdotio',msg);
+    io.emit('chat message', 'me: ' + msg);
+  });
+
 });
 
 app.use(express.static('public'));
